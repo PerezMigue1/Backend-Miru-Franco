64c96243d1b43658043bb35d26dc50f1bea847a5
PerezMigue1 <20230091@uthh.edu.mx>
2025-11-27 14:02:17 -0600

    fix: deshabilitar versionado para ruta raÃ­z usando VERSION_NEUTRAL

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
+  @Version(VERSION_NEUTRAL)
   getRoot() {
     return {
       message: 'Miru Franco Backend API',
